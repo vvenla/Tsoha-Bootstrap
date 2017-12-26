aade00bf8582c161052820dfb9c7fb5714c7decd
Venla V <venla.viljamaa@helsinki.fi>
2017-12-27 01:50:46 +0200

    Tietokantataulut pystytetty, testidataa lisätty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,3 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (tunnus, salasana) VALUES ('Venla', 'salasana');
+INSERT INTO Tehtava (nimi, luotu, huomioita) VALUES ('Pese uuni', NOW(), 'Osta pesuainetta!');
+INSERT INTO Tehtava (nimi, luotu, deadline, huomioita) VALUES ('Lue tenttiin', NOW(), '2017-12-31', 'Laskaritehtävät!');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,31 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+    id SERIAL PRIMARY KEY,
+    tunnus varchar(30) NOT NULL,
+    salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Tarkeys(
+    id SERIAL PRIMARY KEY,
+    aste integer
+);
+
+CREATE TABLE Luokka(
+    id SERIAL PRIMARY KEY,
+    tarkeysId integer REFERENCES Tarkeys(id),
+    nimi varchar(30) NOT NULL
+);
+
+CREATE TABLE Tehtava(
+    id SERIAL PRIMARY KEY,
+    tarkeysId integer REFERENCES Tarkeys(id),
+    nimi varchar(30) NOT NULL,
+    luotu date,
+    status varchar(20),
+    deadline date,
+    huomioita varchar(90)
+);
+
+CREATE TABLE TehtavaLuokka(
+    tehtavaId integer REFERENCES Tehtava(id),
+    luokkaId integer REFERENCES Luokka(id)
+);
